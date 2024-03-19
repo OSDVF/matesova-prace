77257af6166cfaee5ef7137104130bb54ed008a4
OSDVF <o.s.dv.f@seznam.cz>
2024-03-19 14:45:09 +0100

    Added BASE_URL to env

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.BASE_URL,
   plugins: [
     preact(),
     VitePWA({
@@ -29,5 +30,6 @@ export default defineConfig({
   },
   build: {
     sourcemap: true,
+
   }
 })
